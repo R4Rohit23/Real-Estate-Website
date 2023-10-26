92a121e090e4af13e3b34d94ebf06941ec6244ee
Rohit Chaware <80148306+R4Rohit23@users.noreply.github.com>
2023-10-26 15:56:24 +0530

    ADDED Header Section.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,41 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import About from './pages/About';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
+    element: <Home />,
   },
   {
     path: "/about",
-    element: <About/>,
+    element: <About />,
   },
   {
     path: "/profile",
-    element: <Profile/>,
+    element: <Profile />,
   },
   {
     path: "/sign-in",
-    element: <SignIn/>,
+    element: <SignIn />,
   },
   {
     path: "/sign-up",
-    element: <SignUp/>,
+    element: <SignUp />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element = {<Home/>} />
+      <Route path="/about" element = {<About/>} />
+      <Route path="/sign-in" element = {<SignIn/>} />
+    </Routes>
+  </BrowserRouter>
+);
