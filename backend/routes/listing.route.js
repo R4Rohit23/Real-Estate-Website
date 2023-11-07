91a5b7c91f37541afb80ced9764e9b7ac13c259e
Rohit Chaware <80148306+R4Rohit23@users.noreply.github.com>
2023-11-07 10:56:53 +0530

    Added Search Listing API route.

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -85,6 +85,58 @@ const getListing = async (req, res) => {
   }
 };
 
+const getListings = async (req, res) => {
+  try {
+    // limit the number of listings fetch from the database
+    const limit = parseInt(req.query.limit) || 9;
+
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    let offer = req.query.offer;
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    return res
+      .status(501)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
 // API to crate a listing
 router.post("/create", verifyToken, createListing);
 
@@ -96,5 +148,8 @@ router.post("/update/:id", verifyToken, updateListing);
 
 // API to get the listings
 router.get("/get/:id", getListing);
+
+// API for search functionality
+router.get("/get", getListings);
 
 export default router;
